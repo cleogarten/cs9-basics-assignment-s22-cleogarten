95a2b92521ab870eb6b621f76059d75a4fb3c470
Cleo Garten <103280925+cleogarten@users.noreply.github.com>
2022-05-19 23:30:32 +0000

    i added int to the first line, added parenthesis to the 13th line of code, uncapitalized the N in number two in the 13th line,

--- part3.py
+++ part3.py
@@ -7,8 +7,9 @@ There are (at least) 6 errors in this code. Fix them so that it runs properly.
 '''
 
 #code starts here
-number1 = input("Enter a number: ")
+number1 = int(input("Enter a number: "))
 number2 = int(input("Enter another number: ")
 
-print "The sum of your numbers is", number1 + Number2
-print(Seven times your second number is, 7(number2))
+print("The sum of your numbers is", number1 + number2)
+print (Seven times your second number is, 7(number2))
+# i added int to the first line, added parenthesis to the 13th line of code, uncapitalized the N in number two in the 13th line,
